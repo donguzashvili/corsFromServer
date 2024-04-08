064c46b8c9e6dbf514262f68e363ec94095f8a86
Shota Donguzashvili <shota.donguzashvili@flatrocktech.com>
2024-04-08 20:01:21 +0400

    added search

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -11,11 +11,11 @@ app.use(express.json());
 const baseUrl = "https://pro-api.coinmarketcap.com/";
 
 app.get("/latest-currency", async (req, res) => {
-  const { start, limit } = req.query;
+  const { start, limit, id } = req.query;
   const apiKey = req.headers["x-api-key"];
 
   try {
-    const response = await getLatestCurrency({ start, limit, apiKey });
+    const response = await getLatestCurrency({ start, limit, apiKey, id });
     res.json(response.data);
   } catch (error) {
     console.error(error.response.data);
@@ -37,8 +37,10 @@ app.get("/convert-currency", async (req, res) => {
   }
 });
 
-const getLatestCurrency = async ({ start, limit, apiKey }) => {
-  const url = `${baseUrl}v1/cryptocurrency/listings/latest?start=${start}&limit=${limit}`;
+const getLatestCurrency = async ({ start, limit, apiKey, id }) => {
+  const queryString = `v1/cryptocurrency/listings/latest?start=${start}&limit=${limit}${id ? `&convert_id=${id}` : ""}`;
+  const url = `${baseUrl}${queryString}`;
+
   const headers = {
     "X-CMC_PRO_API_KEY": apiKey,
     Accept: "application/json",
@@ -46,11 +48,15 @@ const getLatestCurrency = async ({ start, limit, apiKey }) => {
   };
 
   const response = await axios.get(url, { headers });
+  console.log(id);
+  console.log(response.data);
+
   return response.data;
 };
 
 const getConvertCurrency = async ({ convertFromId, convertToSymbol, apiKey }) => {
-  const url = `${baseUrl}v1/cryptocurrency/quotes/latest?id=${convertFromId}&convert=${convertToSymbol}`;
+  const url = `${baseUrl}v1/cryptocurrency/quotes/latest?id=${convertFromId}${convertToSymbol ? `&convert=${convertToSymbol}` : ""}`;
+  console.log(url);
   const headers = {
     "X-CMC_PRO_API_KEY": apiKey,
     Accept: "application/json",
@@ -58,6 +64,7 @@ const getConvertCurrency = async ({ convertFromId, convertToSymbol, apiKey }) =>
   };
 
   const response = await axios.get(url, { headers });
+  console.log(response);
   return response.data;
 };
 
